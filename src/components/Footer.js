37809da8f290e60cdb0cda244fe5e9ad661a7932
Morozova Valeriya <sandy.lera@yandex.ru>
2023-04-05 19:55:21 +0300

    year

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__author">© 2022 Mesto Russia</p>
+      <p className="footer__author">© 2023 Mesto Russia</p>
     </footer>
   );
 }
